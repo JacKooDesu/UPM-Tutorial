80156291c97ee1c3f2a00e8f651ceb75e88f75ba
JackooDesu <45838705+JacKooDesu@users.noreply.github.com>
2023-09-06 00:06:57 +0800

    add tag

--- Assets/Colorful/Scripts/Colorize.cs
+++ Assets/Colorful/Scripts/Colorize.cs
@@ -7,7 +7,7 @@ namespace Unknow.Colorful
         public static string Tint(this string str, Color color)
         {
             var tag = ColorUtility.ToHtmlStringRGB(color);
-            return $"<color={tag}>{str}</color>";
+            return $"<color=#{tag}>{str}</color>";
         }
     }
 }
